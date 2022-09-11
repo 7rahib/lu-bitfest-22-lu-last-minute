4d71b34a5efcf466b28936acf76b3eac9a6bbb24
7rahib <rahibrashid002@gmail.com>
2022-09-11 12:12:37 +0600

    packages installed

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1 className="">
+        Hello world!
+      </h1>
+      <button className="btn">Button</button>
+    </div>
+  );
+}
+
+export default App;
